38a1c405600f7d2725862d5634cd84d8496296ed
AaronJohnston <aaron.johnston001@gmail.com>
2015-03-20 00:45:32 -0700

    Player, Wumpus, and Minion Stubs 1
    Added stubs for the methods of Player, Wumpus, and Minion, as well as
    attributes for each class. Refer to the included documents for
    specifications about what each class does.

---
+++ WumpusTest_Player_Wumpus_Minion/WumpusTest_Player_Wumpus_Minion/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WumpusTest_Player_Wumpus_Minion
+{
+    partial class Player_Wumpus_Minion_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PlayerButton = new System.Windows.Forms.Button();
+            this.WumpusButton = new System.Windows.Forms.Button();
+            this.MinionButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PlayerButton
+            // 
+            this.PlayerButton.Location = new System.Drawing.Point(139, 168);
+            this.PlayerButton.Name = "PlayerButton";
+            this.PlayerButton.Size = new System.Drawing.Size(133, 23);
+            this.PlayerButton.TabIndex = 0;
+            this.PlayerButton.Text = "Player Class";
+            this.PlayerButton.UseVisualStyleBackColor = true;
+            this.PlayerButton.Click += new System.EventHandler(this.PlayerButton_Click);
+            // 
+            // WumpusButton
+            // 
+            this.WumpusButton.Location = new System.Drawing.Point(139, 197);
+            this.WumpusButton.Name = "WumpusButton";
+            this.WumpusButton.Size = new System.Drawing.Size(133, 23);
+            this.WumpusButton.TabIndex = 1;
+            this.WumpusButton.Text = "Wumpus Class";
+            this.WumpusButton.UseVisualStyleBackColor = true;
+            this.WumpusButton.Click += new System.EventHandler(this.WumpusButton_Click);
+            // 
+            // MinionButton
+            // 
+            this.MinionButton.Location = new System.Drawing.Point(139, 226);
+            this.MinionButton.Name = "MinionButton";
+            this.MinionButton.Size = new System.Drawing.Size(133, 23);
+            this.MinionButton.TabIndex = 2;
+            this.MinionButton.Text = "Minion Class";
+            this.MinionButton.UseVisualStyleBackColor = true;
+            this.MinionButton.Click += new System.EventHandler(this.MinionButton_Click);
+            // 
+            // Player_Wumpus_Minion_Form
+            // 
+            this.BackColor = System.Drawing.Color.SlateBlue;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.MinionButton);
+            this.Controls.Add(this.WumpusButton);
+            this.Controls.Add(this.PlayerButton);
+            this.Name = "Player_Wumpus_Minion_Form";
+            this.Text = "Player Wumpus and Minion Tester";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button PlayerButton;
+        private System.Windows.Forms.Button WumpusButton;
+        private System.Windows.Forms.Button MinionButton;
+    }
+}
+
---
+++ WumpusTest_Player_Wumpus_Minion/WumpusTest_Player_Wumpus_Minion/Minion.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WumpusTest_Player_Wumpus_Minion
+{
+    class Minion
+    {
+        private int position, direction, turns;
+        private String state;
+        private Boolean seesPlayer;
+        private Random rand;
+
+        public Minion(int startingPosition, int startingDirection)
+        {
+            position = startingPosition;
+            direction = startingDirection;
+            turns = 0;
+            state = "roaming";
+        }
+
+        public void moveInDirection(int moveDirection)
+        {
+            // Call method in Map object to see if moving that direction is possible:
+
+            /* int newPosition = _Map.calculateMovement(position, moveDirection); */
+
+            // If possible, update the class:
+
+            /* if(newPosition != -1)
+             * {
+             *      position = newPosition;
+             *      direction = moveDirection;
+             * }
+             */
+        }
+
+        public void addTurn()
+        {
+            turns++;
+        }
+
+        public void updateState()
+        {
+            /* seesPlayer = checkVisible(Player.getPosition()); */
+            if(state.Equals("roaming"))
+            {
+                if(turns % 2 == 0)
+                {
+                    int attemptedDirection = rand.Next(0, 6);
+                    /* int attemptedPosition = calculateMovement(position, attemptedDirection); */
+                    /* if(attemptedPosition != -1)
+                    {
+                        position = attemptedPosition;
+                    } */
+                }
+
+                if(seesPlayer)
+                {
+                    state = "hunting";
+                    turns = 0;
+                }
+            }
+            if(state.Equals("hunting"))
+            {
+                if(turns == 6)
+                {
+                    state = "roaming";
+                    turns = 0;
+                }
+                if(seesPlayer)
+                {
+                    // To be implemented.
+                }
+
+            }
+        }
+
+        public Boolean checkVisible(int checkPosition)
+        {
+            // To be implemented.
+            return true;
+        }
+    }
+}
